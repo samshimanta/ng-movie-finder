bbe2e3566f65384945ca54176a6cdc1e764a8807
sams-shimanta <sams.shimanta@byslglobal.com>
2025-02-10 14:43:11 +0600

    feature:dark mode light mode theme switching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,27 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'ng-movie-finder';
+  darkMode = signal<boolean>(false)
+  @HostBinding('class.dark') get mode(){
+    return this.darkMode()
+  }
+
+
+  setDarkTheme(){
+    console.log(document.documentElement.setAttribute('data-theme','dark'));
+    console.log(this.darkMode());
+     if(!this.darkMode()){
+      document.documentElement.setAttribute('data-theme','dark')
+     } else {
+      document.documentElement.removeAttribute('data-theme')
+      console.log("hio");
+      
+     }
+    
+    
+    console.log(document.documentElement);
+    
+    // document.documentElement.parentNode
+    this.darkMode.set(!this.darkMode())
+  }
 }
